7b2153204734a7382dfe4069e222b04e163058ae
saanviiyer <saanvi.iyer@icloud.com>
2024-06-27 16:58:20 -0700

    used monocle and garnett to isolate a partition

--- practice_monocle.R
+++ practice_monocle.R
@@ -35,14 +35,24 @@ cds <- align_cds(cds, num_dim = 100, alignment_group = "plate")
 cds <- reduce_dimension(cds)
 plot_cells(cds, color_cells_by="plate", label_cell_groups=FALSE)
 
+cds <- cluster_cells(cds, resolution=1e-5)
+plot_cells(cds)
+
 # visualize partition
 plot_cells(cds, color_cells_by="partition", group_cells_by="partition")
 plot_cells(cds, color_cells_by="cao_cell_type")
+plot_cells(cds, color_cells_by="cao_cell_type", label_groups_by_cluster=FALSE)
 
+# find marker genes expressed by each cluster
 marker_test_res <- top_markers(cds, group_cells_by="partition", 
                                reference_cells=1000, cores=8)
 
+top_specific_markers <- marker_test_res %>%
+  filter(fraction_expressing >= 0.10) %>%
+  group_by(cell_group) %>%
+  top_n(1, pseudo_R2)
 
+top_specific_marker_ids <- unique(top_specific_markers %>% pull(gene_id))
 
 # plot expression and partitions
 plot_genes_by_group(cds,
@@ -50,3 +60,63 @@ plot_genes_by_group(cds,
                     group_cells_by="partition",
                     ordering_type="maximal_on_diag",
                     max.size=3)
+
+# ------------------------------------------------------------------------------
+# It's often informative to look at more than one marker, which you can do just by changing the first argument to top_n():
+# top_specific_markers <- marker_test_res %>%
+#                             filter(fraction_expressing >= 0.10) %>%
+#                             group_by(cell_group) %>%
+#                             top_n(3, pseudo_R2)
+# 
+# top_specific_marker_ids <- unique(top_specific_markers %>% pull(gene_id))
+# 
+# plot_genes_by_group(cds,
+#                     top_specific_marker_ids,
+#                     group_cells_by="partition",
+#                     ordering_type="cluster_row_col",
+#                     max.size=3)
+
+# annotate cells according to type
+colData(cds)$assigned_cell_type <- as.character(partitions(cds))
+
+  # remap each cluster to a different cell type
+colData(cds)$assigned_cell_type <- dplyr::recode(colData(cds)$assigned_cell_type,
+                                                 "1"="Body wall muscle",
+                                                 "2"="Germline",
+                                                 "3"="Motor neurons",
+                                                 "4"="Seam cells",
+                                                 "5"="Sex myoblasts",
+                                                 "6"="Socket cells",
+                                                 "7"="Marginal_cell",
+                                                 "8"="Coelomocyte",
+                                                 "9"="Am/PH sheath cells",
+                                                 "10"="Ciliated neurons",
+                                                 "11"="Intestinal/rectal muscle",
+                                                 "12"="Excretory gland",
+                                                 "13"="Chemosensory neurons",
+                                                 "14"="Interneurons",
+                                                 "15"="Unclassified eurons",
+                                                 "16"="Ciliated neurons",
+                                                 "17"="Pharyngeal gland cells",
+                                                 "18"="Unclassified neurons",
+                                                 "19"="Chemosensory neurons",
+                                                 "20"="Ciliated neurons",
+                                                 "21"="Ciliated neurons",
+                                                 "22"="Inner labial neuron",
+                                                 "23"="Ciliated neurons",
+                                                 "24"="Ciliated neurons",
+                                                 "25"="Ciliated neurons",
+                                                 "26"="Hypodermal cells",
+                                                 "27"="Mesodermal cells",
+                                                 "28"="Motor neurons",
+                                                 "29"="Pharyngeal gland cells",
+                                                 "30"="Ciliated neurons",
+                                                 "31"="Excretory cells",
+                                                 "32"="Amphid neuron",
+                                                 "33"="Pharyngeal muscle")
+plot_cells(cds, group_cells_by="partition", color_cells_by="assigned_cell_type")
+
+# look at one partition isolated
+cds_subset <- choose_cells(cds)
+
+
